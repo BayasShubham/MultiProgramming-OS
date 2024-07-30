0a8b74b45c4239ea7ca2b598e31ed556eb9ac066
Shubham Bayas <91380000+BayasShubham@users.noreply.github.com>
2024-07-31 00:17:07 +0530

    Remove incorrect folder phase 2/Phase2

--- phase 2/Phase2/src/Helper.java
+++
@@ -1,9 +0,0 @@
-
-// class Helper{
-//     public void seeInternals(String msg, string val){
-//         System.out.println(msg + ": " + val);
-//     }
-    
-
-
-// }
--- phase 2/Phase2/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        SPARS spars = new SPARS("input_phase2.txt", "output.txt");
-        System.out.println("\nOS initialized");
-        spars.init();
-        spars.LOAD();
-    }
-}
